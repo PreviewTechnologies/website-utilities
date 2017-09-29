1e352c4aeae633f447dde821bbee991c5dea3a3c
Shaharia Azam <shaharia.azam@gmail.com>
2017-09-29 23:25:24 +0600

    Initial commit

---
+++ src/RobotsTxtGenerator/Generator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Previewtechs\WebsiteUtilities\RobotsTxtGenerator;
+
+
+class Generator
+{
+
+}
---
+++ src/SitemapGenerator/Sitemap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Write something about the purpose of this file
+ *
+ * @author Shaharia Azam <shaharia@previewtechs.com>
+ * @url https://www.previewtechs.com
+ */
+
+namespace Previewtechs\WebsiteUtilities\SitemapGenerator;
+
+
+class Sitemap
+{
+
+}
